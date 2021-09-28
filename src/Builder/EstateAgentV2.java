3ae98c8542962f2e215682c3cee04dddb7a87ad2
Can <muhammetcanozyigit@gmail.com>
2021-09-28 16:02:57 +0300

    commit

--- src/Builder/EstateAgentV2.java
+++ src/Builder/EstateAgentV2.java
@@ -8,6 +8,17 @@ public class EstateAgentV2 {
                 .build();
 
 
+        Home home1 = HomeBuilder.startBuild("Sivas","Merkez","Mevlana",6,2019)
+                        .build();
+
+        Home home2 = HomeBuilder.startBuild("Sivas","Merkez","Kılavuz",5,2015)
+                        .setDublex(false)
+                        .setPool(false)
+                        .setBathCount(3)
+                        .setHasCarPax(true)
+                        .build();
         System.out.println(home);
+        System.out.println(home1);
+        System.out.println(home2);
     }
 }
